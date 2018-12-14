f0d15e6f02531caf4467e7d6b6a98a4e413c23f7
drazige <gdrazick@yahoo.com>
2018-12-14 10:13:35 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6houVCMQSy+5GogHWx0Lq5C2ORuCjrm7Yu9Fi+ixx/GiSZ7TcgHz17Qu33ph7mj1JrMhisW2kN7cvxVfqnsI423YaS98iorf0Dbt1V3qONbJyRHoRTOph7dwTCigdG9oFdDqH4gb1ak1T891vHwoevfkE6Mhqr8ygAQ/u63eEy5IWUJJZ/zd1ImWzXaCnQ+YtwmNtv/y8SchrfVxXWpEpRHYpUF07q+jmcivhLOjmqJB6pLR+R/D5RLglqJL+z2pcwqKdAFByn3sbUOsmPzEmTk0VE+LUONctHT5QUhOpMXk4wDmKo2f6LPBZtXyK9cYV781DXiW5KBkWMgNuFEvj root@master.puppet.vm',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC6houVCMQSy+5GogHWx0Lq5C2ORuCjrm7Yu9Fi+ixx/GiSZ7TcgHz17Qu33ph7mj1JrMhisW2kN7cvxVfqnsI423YaS98iorf0Dbt1V3qONbJyRHoRTOph7dwTCigdG9oFdDqH4gb1ak1T891vHwoevfkE6Mhqr8ygAQ/u63eEy5IWUJJZ/zd1ImWzXaCnQ+YtwmNtv/y8SchrfVxXWpEpRHYpUF07q+jmcivhLOjmqJB6pLR+R/D5RLglqJL+z2pcwqKdAFByn3sbUOsmPzEmTk0VE+LUONctHT5QUhOpMXk4wDmKo2f6LPBZtXyK9cYV781DXiW5KBkWMgNuFEvj',
   }  
 }
